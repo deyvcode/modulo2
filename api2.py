3daf5ee48221c80d4aa4996db6727a00595c955c
deyvcode <deyvidwilliansalesdearaujo@gmail.com>
2025-10-02 17:09:09 -0300

    Add files via upload

---
+++ api2.py
@@ -0,0 +1,12 @@
+import requests
+
+prato=input('Digite o prato que você deseja: ')
+bebida=input('Digite a bebida que você deseja: ')
+mesa=int(input('Digite a mesa a qual você deseja se sentar: '))
+
+pedido={
+    'Prato': prato,
+    'Bebida': bebida,
+    'Mesa': mesa
+}
+requests.post('https://68dd8c16d7b591b4b78cc26a.mockapi.io/Restaurante', pedido )
